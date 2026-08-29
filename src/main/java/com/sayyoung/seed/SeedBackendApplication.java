8b091eea65c8513caef14cc2da06a022b2d28607
ksy <ksy9565@gmail.com>
2026-08-29 16:20:08 +0900

    chore: 프로젝트 초기 세팅[#1]
    Dependencies:
    JPA, MySQL, Redis, Spring Security, Thymeleaf,
    validation, WebMVC, log4j2, Swagger, Lombok

---
+++ src/main/java/com/sayyoung/seed/SeedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sayyoung.seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayyoung/seed/SeedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayyoung.seed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
