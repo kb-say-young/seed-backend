b2b1efe21255cdf8156754587b8ae5ba542be8ab
hoon <stitch8971@gachon.ac.kr>
2026-08-31 00:58:44 +0900

    chore: Spring Boot 프로젝트 기본 설정 정리

--- src/main/java/com/sayyoung/seed/SeedApplication.java
+++ src/main/java/com/sayyoung/seed/SeedApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SeedBackendApplication {
+public class SeedApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeedBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SeedApplication.class, args);
+    }
 
 }
